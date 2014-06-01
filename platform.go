fc41e106ee0e4394f69a56046e5c5adbaf29d912
Damian Gryski <damian@gryski.com>
2014-06-01 22:03:37 +0200

    inline uint64 unpacking

--- platform.go
+++ platform.go
@@ -1,9 +1,5 @@
 package farm
 
-import (
-	"encoding/binary"
-)
-
 func rotate32(val uint32, shift uint) uint32 {
 	// Avoid shifting by 32: doing so yields an undefined result.
 	if shift == 0 {
@@ -21,9 +17,10 @@ func rotate64(val uint64, shift uint) uint64 {
 }
 
 func fetch32(s []byte, idx int) uint32 {
-	return binary.LittleEndian.Uint32(s[idx:])
+	return uint32(s[idx+0]) | uint32(s[idx+1])<<8 | uint32(s[idx+2])<<16 | uint32(s[idx+3])<<24
 }
 
 func fetch64(s []byte, idx int) uint64 {
-	return binary.LittleEndian.Uint64(s[idx:])
+	return uint64(s[idx+0]) | uint64(s[idx+1])<<8 | uint64(s[idx+2])<<16 | uint64(s[idx+3])<<24 |
+		uint64(s[idx+4])<<32 | uint64(s[idx+5])<<40 | uint64(s[idx+6])<<48 | uint64(s[idx+7])<<56
 }
