ee51083b50a3d5e12d147e40d75dbdc02283ee10
nicolaasuni <nicola@cognitivelogic.com>
2016-02-09 16:11:35 +0000

    improved benchmarks

--- bench_test.go
+++ bench_test.go
@@ -2,20 +2,35 @@ package farm
 
 import "testing"
 
-func BenchmarkShort(b *testing.B) {
+var res32 uint32
+var res64 uint64
+var res64lo, res64hi uint64
 
-	buf := make([]byte, 32)
+var buf = make([]byte, 256)
 
+func BenchmarkHash32(b *testing.B) {
+	var r uint32
 	for i := 0; i < b.N; i++ {
-		Hash32(buf)
+		// record the result to prevent the compiler eliminating the function call
+		r = Hash32(buf)
 	}
+	// store the result to a package level variable so the compiler cannot eliminate the Benchmark itself
+	res32 = r
 }
 
 func BenchmarkHash64(b *testing.B) {
-
-	buf := make([]byte, 2048)
-
+	var r uint64
 	for i := 0; i < b.N; i++ {
-		Hash64(buf)
+		r = Hash64(buf)
 	}
+	res64 = r
+}
+
+func BenchmarkHash128(b *testing.B) {
+	var rlo, rhi uint64
+	for i := 0; i < b.N; i++ {
+		rlo, rhi = Hash128(buf)
+	}
+	res64lo = rlo
+	res64hi = rhi
 }
