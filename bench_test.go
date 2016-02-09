7a4068ef99fd5dbabf4a830df77c0b9e875d3cb2
nicolaasuni <nicola@cognitivelogic.com>
2016-02-09 16:24:45 +0000

    use random-generated string on benchmark

--- bench_test.go
+++ bench_test.go
@@ -6,7 +6,8 @@ var res32 uint32
 var res64 uint64
 var res64lo, res64hi uint64
 
-var buf = make([]byte, 256)
+// 256-bytes random string
+var buf = []byte("RMVx)@MLxH9M.WeGW-ktWwR3Cy1XS.,K~i@n-Y+!!yx4?AB%cM~l/#0=2:BOn7HPipG&o/6Qe<hU;$w1-~bU4Q7N&yk/8*Zz.Yg?zl9bVH/pXs6Bq^VdW#Z)NH!GcnH-UesRd@gDij?luVQ3;YHaQ<~SBm17G9;RWvGlsV7tpe*RCe=,?$nE1u9zvjd+rBMu7_Rg4)2AeWs^aaBr&FkC#rcwQ.L->I+Da7Qt~!C^cB2wq(^FGyB?kGQpd(G8I.A7")
 
 func BenchmarkHash32(b *testing.B) {
 	var r uint32
